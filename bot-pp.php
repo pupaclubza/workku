dc00fa28cd671c521c3c581addcf4e77e749798f
pupaclubza <pupa_club@hotmail.com>
2019-09-16 11:38:16 +0700

    bot update

--- bot-pp.php
+++ bot-pp.php
@@ -4,26 +4,43 @@
 
 
 
-//ดึง class จากพวก line
-use LINE\LINEBot;
-use LINE\LINEBot\HTTPClient;
-use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+  //ดึง class จากพวก line
+  use LINE\LINEBot;
+  use LINE\LINEBot\HTTPClient;
+  use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+  use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
+  //ดึงโทเค็นมาใช้
+  $httpClient = new CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
+  //คือสร้าง array เก็บเลขห้อง
+  $bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SECRET));
 
-//ดึงโทเค็นมาใช้
-$httpClient = new CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
-//คือสร้าง array เก็บเลขห้อง
-$bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SECRET));
+  // คำสั่งรอรับการส่งค่ามาของ LINE Messaging API
+  $content = file_get_contents('php://input');
 
-// คำสั่งรอรับการส่งค่ามาของ LINE Messaging API
-$content = file_get_contents('php://input');
+  // แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
+  $events = json_decode($content, true);
 
-// แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
-$events = json_decode($content, true);
-if(!is_null($events)){
+    if(!is_null($events)){
+        $replyToken  = $events['events'][0]['replyToken'];
+        $typeMessage = $events['events'][0]['message']['type'];
 
-}
-echo "line OK if ok";
+        switch ($typeMessage){
+                case 'text':
+                        switch ($userMessage){
+                                case "id":
+                                          $replyData = new TextMessageBuilder("Your ID: ".$events['events'][0]['source']['userId']);
+                                          break;
+                        }
+        }
+
+  $response = $bot->replyMessage($replyToken,$replyData);
+    if ($response->isSucceeded()) {
+            echo 'Succeeded!';
+            return;
+      }
+
+echo "line OK if ok ok";
 
 
  ?>
