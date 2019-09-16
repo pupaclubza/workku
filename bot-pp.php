0045b71eaa732fa9c0255cac0fa4ac159429cf1b
pupaclubza <pupa_club@hotmail.com>
2019-09-16 10:19:47 +0700

    bot

--- bot-pp.php
+++ bot-pp.php
@@ -18,10 +18,7 @@ $content = file_get_contents('php://input');
 
 // แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
 $events = json_decode($content, true);
+echo "line OK";
 
-if(!is_null($events)){
 
-
-}
-  echo "line OK";
  ?>
