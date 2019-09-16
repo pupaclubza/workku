c035f1c590a08cbfe7ccf9ed2dbde54dab05f539
pupaclubza <pupa_club@hotmail.com>
2019-09-16 09:31:25 +0700

    bot-line

---
+++ bot_settings.php
@@ -0,0 +1,6 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','1579476958');
+define('LINE_MESSAGE_CHANNEL_SECRET','2b33f6133fe6db4800724c11b368488e');
+define('LINE_MESSAGE_ACCESS_TOKEN','dfFBryGM9oPRn6P5+1Ch4+C1KsjG64BeuizmGbWWc0SRkGTPElLrq+tieVWwUzza9rHVmqcaYRnuA//xeH9aiB5v7yjvhJh2qshADvftSlvQ4pBEhRLDiWE8ozwL6upD/8ssl6cPqQaWo33YhnQJewdB04t89/1O/w1cDnyilFU=');
+?>
