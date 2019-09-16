d689fff7199f66cc55b64c2baec2d8e80e651b15
pupaclubza <pupa_club@hotmail.com>
2019-09-16 13:33:16 +0700

    set

--- bot_settings.php
+++ bot_settings.php
@@ -2,6 +2,6 @@
 /// ��õ�駤�������ǡѺ bot �� LINE Messaging API
 define('LINE_MESSAGE_CHANNEL_ID','1620990431'); //เอาจากใน line Devellopers
 define('LINE_MESSAGE_CHANNEL_SECRET','7be83b7c959cdd5bb69d1fd278f83549'); //เอาจากใน line Devellopers
-define('LINE_MESSAGE_ACCESS_TOKEN','s58zJCt3XjR2f+2O2F2C54bnUHhfsLw/U39XU8NBV8R/Ur+qZQRJpVKIYTJtoawVSMru23iJUapenwGS/BzFNy/LqR+p1UaNyxZIkyBFauVZJ59xC4cDG2F/BtHURZmgykfro3yZvPz7JslAeK2RxwdB04t89/1O/w1cDnyilFU=');
+define('LINE_MESSAGE_ACCESS_TOKEN','2MiPJwII9/05Z13ynbalhYcKDyHJtZfkjT9thAmG25V/52w8wkMP3KsIhvZOKWIBSMru23iJUapenwGS/BzFNy/LqR+p1UaNyxZIkyBFauVFvLg45RW6KRcO2S660V5VO4GDlOQfYdeRTpjlL+cwwwdB04t89/1O/w1cDnyilFU=');
 //เอาจากใน line Devellopers
 ?>
